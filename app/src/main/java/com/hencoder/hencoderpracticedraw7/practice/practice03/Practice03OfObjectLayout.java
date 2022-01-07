461896ffd47efef4a5ac43ef2e876853c78dc221
luyao <luyao@douyu.tv>
2022-01-07 10:10:58 +0800

    【自定义View1-7】作业3提交

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
@@ -5,6 +5,7 @@ import android.animation.TypeEvaluator;
 import android.content.Context;
 import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
@@ -39,7 +40,7 @@ public class Practice03OfObjectLayout extends RelativeLayout {
             @Override
             public void onClick(View v) {
                 ObjectAnimator animator = ObjectAnimator.ofObject(view, "position",
-                        new PointFEvaluator(), new PointF(0, 0), new PointF(1, 1));
+                        new PointFEvaluator(), new PointF(0, 0), new PointF(2, 2));
                 animator.setInterpolator(new LinearInterpolator());
                 animator.setDuration(1000);
                 animator.start();
@@ -49,10 +50,20 @@ public class Practice03OfObjectLayout extends RelativeLayout {
 
     private class PointFEvaluator implements TypeEvaluator<PointF> {
 
-        // 重写 evaluate() 方法，让 PointF 可以作为属性来做动画
+        /**
+         * 重写 evaluate() 方法，让 PointF 可以作为属性来做动画
+         * @param fraction：当前动画完成度，从0，0.01,0.02...一直到1，将近100个
+         * @param startValue：ObjectAnimator.ofObject的参数第一个
+         * @param endValue：ObjectAnimator.ofObject的参数第二个
+         * @return：当动画的进度为 fraction 时，对应的PointF对象
+         */
         @Override
         public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
-            return startValue;
+            float x = startValue.x + (endValue.x - startValue.x) * fraction;
+            float y = startValue.y + (endValue.y - startValue.y) * fraction;
+            Log.e("luy", "fraction:" + fraction + ",start:[" + startValue.x + "," + startValue.y + "]" +
+                    ", end:[" + endValue.x + "," + endValue.y + "]" + x + " + y:" + y);
+            return new PointF(x, y);
         }
     }
 }
