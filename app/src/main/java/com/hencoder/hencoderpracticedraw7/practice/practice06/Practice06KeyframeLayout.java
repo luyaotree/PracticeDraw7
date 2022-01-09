72fc62fcac3496c77cd47788e16fdfefda5c32bc
luyao <luyao@douyu.tv>
2022-01-09 15:18:56 +0800

    【自定义View1-7】作业6提交

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice06/Practice06KeyframeLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice06/Practice06KeyframeLayout.java
@@ -1,8 +1,13 @@
 package com.hencoder.hencoderpracticedraw7.practice.practice06;
 
+import android.animation.Keyframe;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.content.Context;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
@@ -40,6 +45,18 @@ public class Practice06KeyframeLayout extends RelativeLayout {
                 // 结束帧：progress 回落到 80
                 // 使用 PropertyValuesHolder.ofKeyframe() 来把关键帧拼接成一个完整的属性动画方案
                 // 使用 ObjectAnimator.ofPropertyValuesHolder() 来创建动画
+
+                // 在 0% 处开始
+                Keyframe keyframe1 = Keyframe.ofFloat(0f, 0);
+                // 时间经过 50% 的时候，动画完成度 100%
+                Keyframe keyframe2 = Keyframe.ofFloat(0.5f, 100f);
+                // 时间经过 100% 的时候，动画完成度倒退到 80%，即反弹 20%
+                Keyframe keyframe3 = Keyframe.ofFloat(1, 80f);
+                PropertyValuesHolder holder = PropertyValuesHolder.ofKeyframe("progress", keyframe1, keyframe2, keyframe3);
+                ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(view, holder);
+                objectAnimator.setInterpolator(new FastOutSlowInInterpolator());
+                objectAnimator.setDuration(1500);
+                objectAnimator.start();
             }
         });
     }
